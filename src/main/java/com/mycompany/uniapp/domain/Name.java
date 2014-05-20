ceb73381ab970b0593418e9a3c3d85c38d817814
Jill <Jill@Jill-PC>
2014-05-20 15:53:04 +0200

    Name commit

--- src/main/java/com/mycompany/uniapp/domain/Name.java
+++ src/main/java/com/mycompany/uniapp/domain/Name.java
@@ -5,53 +5,33 @@
 package com.mycompany.uniapp.domain;
 
 import java.io.Serializable;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.Embeddable;
 
 /**
  *
  * @author Jill
  */
-@Entity
-public class Name implements Serializable {
-    private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+@Embeddable
+public class Name implements Serializable{
+    
+    private String firstName;
+    private String lastName;
 
-    public Long getId() {
-        return id;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
+    public String getLastName() {
+        return lastName;
     }
 
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Name)) {
-            return false;
-        }
-        Name other = (Name) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
-
-    @Override
-    public String toString() {
-        return "com.mycompany.uniapp.domain.Name[ id=" + id + " ]";
-    }
+    
     
 }
